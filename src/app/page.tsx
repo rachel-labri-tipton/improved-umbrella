5ddacf32f00fe726c1e2368360dfe2274d571913
Rachel Tipton <rachelmtipton@gmail.com>
2025-05-27 18:28:36 +0200

    update hero banner media queries

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import styles from './page.module.scss';
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <div className={styles.hero}>
+        <h2 className={styles.title}>rachel l'abri tipton</h2>
+        <h1 className={styles.title}>FULL STACK DEVELOPER</h1>
+      </div>
+    </div>
+  );
+}
