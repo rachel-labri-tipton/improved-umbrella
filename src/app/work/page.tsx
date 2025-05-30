7f829489e4c74e56756efcc4e8843db8906daeda
Rachel Tipton <rachelmtipton@gmail.com>
2025-05-30 14:37:16 +0200

    adds page for work and projects

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,33 +1,31 @@
-import React from 'react';
-import Carousel from '@/components/Carousel/Carousel';
+import React, { Suspense } from 'react';
 import styles from './work.module.scss';
-import carouselStyles from '@/components/Carousel/Carousel.module.scss';
-import ProjectCard from '@/components/ProjectCard/ProjectCard';
-import mockProjects from '@/data/mockData';
+import ExperienceContainer from '@/components/ExperienceContainer/ExperienceContainer';
+import { CircularProgress } from '@mui/material';
+import mockData from '@/data/mockData';
+import { ProjectCardProps } from '@/components/ProjectCard/ProjectCard';
 
 const Work: React.FC = () => {
   return (
     <div className={styles.work}>
-      {/* heading container */}
       <div className="sectionHeadingWrapper">
         <div className="sectionHeading">
           <h1>work & projects</h1>
         </div>
       </div>
-      <div className={styles.carouselWrapper}>
-        {Object.entries(mockProjects).map(([category, projects]) => (
-          <div key={category} className={styles.carouselContainer}>
-            <h2 className={carouselStyles.carouselHeading}>{category}</h2>{' '}
-            {/* Format category name */}
-            <Carousel>
-              {projects.map((project) => (
-                <div key={project.id} className={styles.carouselItem}>
-                  <ProjectCard {...project} />
-                </div>
-              ))}
-            </Carousel>
-          </div>
-        ))}
+      <div>
+        <Suspense fallback={<CircularProgress />}>
+          {mockData.map((item, index) => (
+            <ExperienceContainer
+              key={index}
+              companyName={item.companyName}
+              role={item.role}
+              period={item.period}
+              description={item.description}
+              projects={item.projects as ProjectCardProps[]}
+            />
+          ))}
+        </Suspense>
       </div>
     </div>
   );
